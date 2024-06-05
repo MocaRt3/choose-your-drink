885ab6ce93254ca5f579a3e01e2e92b9fc720866
MocaRt12 <nikitadubovik612@gmail.com>
2024-06-05 19:38:31 +0300

    change configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: '/choose-your-drink-vue-/'
+  publicPath: '/choose-your-drink/'
 })
